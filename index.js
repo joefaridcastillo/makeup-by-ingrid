eac03bdc9908c583c97e7495423b8711faa2f27d
Joe F Castillo <joefcastillo@Joes-MacBook-Pro.local>
2020-09-10 20:01:00 -0400

    Added book.html, worked on booking section

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ var modal = document.getElementById("myModal");
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById("myImg");
 var imgOne = document.getElementById("myImgOne");
+var imgTwo = document.getElementById("myImgTwo");
+var imgThree = document.getElementById("myImgThree");
+var imgFour = document.getElementById("myImgFour");
+var imgFive = document.getElementById("myImgFive");
+var imgSix = document.getElementById("myImgSix");
 
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
@@ -21,6 +26,31 @@ img.onclick = function(){
   captionText.innerHTML = this.alt;
 }
 imgOne.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+imgTwo.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+imgThree.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+imgFour.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+imgFive.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+imgSix.onclick = function(){
   modal.style.display = "block";
   modalImg.src = this.src;
   captionText.innerHTML = this.alt;
